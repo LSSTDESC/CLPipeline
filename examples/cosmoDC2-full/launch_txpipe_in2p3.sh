a0345ffc99be8c4191813d6814fd9ffc12ba112b
eduardojsbarroso <eduardojsbarroso@gmail.com>
2025-04-25 10:11:36 +0200

    Added full cosmoDC2 example and fixed TJPCov stage

---
+++ examples/cosmoDC2-full/launch_txpipe_in2p3.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+#SBATCH --time=13:00:00
+#SBATCH --partition=lsst
+#SBATCH --ntasks=60
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=400gb   
+#SBATCH --nodes=1
+
+source ~/.bashrc
+#source /pbs/throng/lsst/software/desc/common/miniconda/setup_current_python.sh
+conda activate txpipe_clp
+export HDF5_DO_MPI_FILE_SYNC=0
+export PYTHONPATH=/sps/lsst/groups/clusters/cl_pipeline_project/TXPipe:$PYTHONPATH
+export PYTHONPATH=../../:$PYTHONPATH
+ceci CL_cosmoDC2-20deg2_concat_in2p3.yml --yamlId TXPipe 
---
+++ examples/cosmoDC2-full/outputs/launch.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+#SBATCH --time=15:00:00
+#SBATCH --partition=hpc,lsst
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=64gb
+
+source ~/.bashrc
+conda activate firecrown_clp
+cosmosis cluster_counts_mean_mass_redshift_richness.ini
