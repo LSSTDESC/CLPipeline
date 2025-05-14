a5b4e29f2b0161e2b7cb631a24808de2a1fa008e
eduardojsbarroso <eduardojsbarroso@gmail.com>
2025-05-14 11:54:07 +0200

    Added tests for false shear and CLPipelineAuto config

---
+++ examples/cosmoDC2-Txpipe-Shear/440deg2-true-shear/launch_txpipe_in2p3.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+#SBATCH --time=13:00:00
+#SBATCH --partition=hpc,lsst
+#SBATCH --ntasks=60
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=500gb   
+#SBATCH --nodes=1
+
+module load conda
+#source /pbs/throng/lsst/software/desc/common/miniconda/setup_current_python.sh
+conda activate /sps/lsst/groups/clusters/cl_pipeline_project/conda_envs/txpipe_clp
+export HDF5_DO_MPI_FILE_SYNC=0
+export PYTHONPATH=/sps/lsst/groups/clusters/cl_pipeline_project/TXPipe:$PYTHONPATH
+export PYTHONPATH=../../../:$PYTHONPATH
+ceci CL_txpipe_shear_in2p3.yml --yamlId TXPipe 
