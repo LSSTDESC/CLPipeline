acd5ca370c10eb91bf87b7be31f36841e0e92ff2
eduardojsbarroso <eduardojsbarroso@gmail.com>
2025-06-03 20:51:00 +0200

    ii

--- examples/lsst_france/cosmodc2_scaling_relation/launch_job_fire.sh
+++ examples/lsst_france/cosmodc2_scaling_relation/launch_job_fire.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/bash
-#SBATCH --partition=hpc               # Partition choice
+#SBATCH --partition=hpc,lsst               # Partition choice
 #SBATCH --mem=8G                    # Memory in MB per default
 #SBATCH --time 13:00:00             # 7 days by default on htc partition
-#SBATCH --cpus-per-task=8
-#  SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --ntasks=1
 #SBATCH --job-name=cosmosis_emcee
 #SBATCH --output=cosmosis_emcee.out
 #SBATCH --error=cosmosis_emcee.err
-source ~/.bashrc
-#source /pbs/throng/lsst/software/desc/common/miniconda/setup_current_python.sh
-conda activate firecrown_clp
-# Set your project path
 
+module load Programming_Languages/anaconda/3.11
+#source ~/.bashrc
+#source /pbs/throng/lsst/software/desc/common/miniconda/setup_current_python.sh
+conda activate /sps/lsst/users/ebarroso/miniforge3/envs/firecrown_clp
+# Set your project path
+export OMP_NUM_THREADS=1
+pip list emcee
 # Run CosmoSIS
 cd outputs
 export PYTHONPATH=/sps/lsst/users/ebarroso/CLPipeline/:$PYTHONPATH
