c5a5dbf2dcc40d36669c74eaf2a42b4cacd60a06
eduardojsbarroso <eduardojsbarroso@gmail.com>
2025-06-04 13:59:00 +0200

    Corrected file for true example

--- examples/lsst_france/cosmodc2_scaling_relation/outputs/cluster_redshift_richness.py
+++ examples/lsst_france/cosmodc2_scaling_relation/outputs/cluster_redshift_richness.py
@@ -1,8 +1,6 @@
 import os
 import pyccl as ccl
 import sacc
-import sys
-sys.path.insert(0, "/sps/lsst/users/ebarroso/CLPipeline")
 from firecrown.likelihood.gaussian import ConstGaussian
 from firecrown.likelihood.binned_cluster_number_counts import BinnedClusterNumberCounts
 from firecrown.likelihood.likelihood import Likelihood, NamedParameters
