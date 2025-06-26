847f9772e43437b70d371f5d1bbcbe3383aa2fed
eduardojsbarroso <eduardojsbarroso@gmail.com>
2025-06-26 10:24:15 +0200

    Changed some examples

--- examples/lsst_france/cosmodc2_scaling_relation/launch_job_fire.sh
+++ examples/lsst_france/cosmodc2_scaling_relation/launch_job_fire.sh
@@ -8,14 +8,12 @@
 #SBATCH --output=cosmosis_emcee.out
 #SBATCH --error=cosmosis_emcee.err
 
+#module load Programming_Languages/anaconda/3.11
+source /usr/share/Modules/init/bash
 module load Programming_Languages/anaconda/3.11
 #source ~/.bashrc
 #source /pbs/throng/lsst/software/desc/common/miniconda/setup_current_python.sh
-conda activate /sps/lsst/users/ebarroso/miniforge3/envs/firecrown_clp
-# Set your project path
-export OMP_NUM_THREADS=1
-pip list emcee
-# Run CosmoSIS
+conda activate /sps/lsst/users/ebarroso/conda_envs/firecrown_clp #/sps/lsst/groups/clusters/cl_pipeline_project/conda_envs/firecrown_clp
 cd outputs
 export PYTHONPATH=/sps/lsst/users/ebarroso/CLPipeline/:$PYTHONPATH
 cosmosis cluster_counts_mean_mass_redshift_richness.ini
