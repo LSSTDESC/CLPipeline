a3d43ed35c03ffe61239aee34d29c9c64eb5760a
eduardojsbarroso <eduardojsbarroso@gmail.com>
2025-10-22 10:31:00 +0200

    Added example files

---
+++ examples/cosmoDC2_analysis_narei/launch_job_in2p3.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+#SBATCH --time=15:00:00
+#SBATCH --partition=hpc,lsst
+#SBATCH --ntasks=60
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=64gb
+
+module load conda
+conda activate /sps/lsst/groups/clusters/cl_pipeline_project/conda_envs/txpipe_clp
+export HDF5_DO_MPI_FILE_SYNC=0
+export PYTHONPATH=/sps/lsst/groups/clusters/cl_pipeline_project/TXPipe:$PYTHONPATH
+export PYTHONPATH=../../:$PYTHONPATH
+ceci CL_cosmoDC2_concat_in2p3.yml --yamlId TXPipe 
+ceci CL_cosmoDC2_concat_in2p3.yml --yamlId TJPCov
+conda deactivate
+conda activate /sps/lsst/groups/clusters/cl_pipeline_project/conda_envs/firecrown_clp
+export PYTHONPATH=../../:$PYTHONPATH
+ceci CL_cosmoDC2_concat_in2p3.yml --yamlId Firecrown
+cd outputs
+cosmosis cluster_counts_mean_mass_redshift_richness.ini
