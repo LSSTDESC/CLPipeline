d315ce2ee219f9b19724fc1acc52577d53adefe1
eduardojsbarroso <eduardojsbarroso@gmail.com>
2026-01-19 14:18:27 +0100

    Irrelevant files

--- examples/cosmoDC2-20deg2/launch_txpipe_in2p3.sh
+++ examples/cosmoDC2-20deg2/launch_txpipe_in2p3.sh
@@ -1,19 +1,15 @@
 #!/usr/bin/bash
-#SBATCH --time=15:00:00
+#SBATCH --time=3:00:00
 #SBATCH --partition=hpc,lsst
+#SBATCH --ntasks=60
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=128000
+#SBATCH --mem=128gb   
+#SBATCH --nodes=1
 
 source ~/.bashrc
+#source /pbs/throng/lsst/software/desc/common/miniconda/setup_current_python.sh
 conda activate txpipe_clp
 export HDF5_DO_MPI_FILE_SYNC=0
 export PYTHONPATH=/sps/lsst/groups/clusters/cl_pipeline_project/TXPipe:$PYTHONPATH
 export PYTHONPATH=../../:$PYTHONPATH
 ceci CL_cosmoDC2-20deg2_concat_in2p3.yml --yamlId TXPipe 
-ceci CL_cosmoDC2-20deg2_concat_in2p3.yml --yamlId TJPCov
-conda deactivate
-conda activate firecrown_clp
-export PYTHONPATH=../../:$PYTHONPATH
-ceci CL_cosmoDC2-20deg2_concat_in2p3.yml --yamlId Firecrown
-cd outputs
-cosmosis cluster_counts_mean_mass_redshift_richness.ini
