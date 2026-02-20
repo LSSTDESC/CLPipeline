46458038f1400fbfc9b69d0428ee7ad9f6e6554d
eduardojsbarroso <eduardojsbarroso@gmail.com>
2026-02-20 09:55:55 +0100

    Full analysis redmapper and start wazp

---
+++ examples/cosmodc2_redmapper/fixed_concentration/cosmodc2_redmapper_counts/launch_job_cosmo.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+#SBATCH --time=15:00:00
+#SBATCH --partition=hpc,lsst
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=8gb
+#SBATCH --ntasks=1 
+module load conda
+export HDF5_DO_MPI_FILE_SYNC=0
+#export PYTHONPATH=/sps/lsst/groups/clusters/cl_pipeline_project/TXPipe:$PYTHONPATH
+conda activate /sps/lsst/groups/clusters/cl_pipeline_project/conda_envs/txpipe_clp
+export PYTHONPATH=../../../../:$PYTHONPATH
+ceci CL_cosmoDC2-full_concat_cosmo.yml --yamlId TJPCov
+conda deactivate
+conda activate /sps/lsst/groups/clusters/cl_pipeline_project/conda_envs/firecrown_developer_clp
+export PYTHONPATH=../../../../:$PYTHONPATH
+ceci CL_cosmoDC2-full_concat_cosmo.yml --yamlId Firecrown
+cd outputs_cosmo
+cosmosis cluster_counts_mean_mass_redshift_richness.ini
---
+++ examples/cosmodc2_redmapper/fixed_concentration/cosmodc2_redmapper_counts/launch_job_mor.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+#SBATCH --time=15:00:00
+#SBATCH --partition=hpc,lsst
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=8gb
+#SBATCH --ntasks=1 
+module load conda
+export HDF5_DO_MPI_FILE_SYNC=0
+conda activate /sps/lsst/groups/clusters/cl_pipeline_project/conda_envs/txpipe_clp
+export PYTHONPATH=../../../../:$PYTHONPATH
+ceci CL_cosmoDC2-full_concat_mor.yml --yamlId TJPCov
+conda deactivate
+conda activate /sps/lsst/groups/clusters/cl_pipeline_project/conda_envs/firecrown_developer_clp
+export PYTHONPATH=../../../../:$PYTHONPATH
+ceci CL_cosmoDC2-full_concat_mor.yml --yamlId Firecrown
+cd outputs_mor
+cosmosis cluster_counts_mean_mass_redshift_richness.ini
