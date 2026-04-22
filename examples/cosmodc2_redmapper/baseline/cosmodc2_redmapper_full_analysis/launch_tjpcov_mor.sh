c1754cf68cfff068dca90fd8ab168b8611169da3
eduardojsbarroso <eduardojsbarroso@gmail.com>
2026-04-22 18:18:38 +0200

    Final commit

---
+++ examples/cosmodc2_redmapper/baseline/cosmodc2_redmapper_full_analysis/launch_tjpcov_mor.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+#SBATCH --time=15:00:00
+#SBATCH --partition=hpc,lsst
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=8gb
+#SBATCH --ntasks=10 
+module load conda
+export HDF5_DO_MPI_FILE_SYNC=0
+conda activate /sps/lsst/groups/clusters/cl_pipeline_project/conda_envs/txpipe_clp
+export PYTHONPATH=../../../../:$PYTHONPATH
+
+export PYTHONPATH=/sps/lsst/groups/clusters/cl_pipeline_project/TXPipe:$PYTHONPATH
+#ceci CL_cosmoDC2-full_concat_mor.yml --yamlId TXPipe
+ceci CL_cosmoDC2-full_concat_mor.yml --yamlId TJPCov
