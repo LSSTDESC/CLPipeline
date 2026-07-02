c95dac2f9256d663489f3d46250e51695093c224
eduardojsbarroso <eduardojsbarroso@gmail.com>
2026-07-02 16:12:44 +0200

    Changed selection function case, added ingestion stage

---
+++ examples/cosmodc2_redmapper/ingest/README.txt
@@ -0,0 +1,1 @@
+The cosmoDC2 galaxy catalog must only be ingested one. With the configurations here, we have r_mag < 28 and i_mag < 24.25.
---
+++ examples/cosmodc2_redmapper/ingest/launch_ingest_in2p3.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+#SBATCH --time=12:00:00
+#SBATCH --partition=hpc,lsst
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=200gb
+#SBATCH --ntasks=1
+
+module load conda
+export HDF5_DO_MPI_FILE_SYNC=0
+conda activate /sps/lsst/groups/clusters/cl_pipeline_project/conda_envs/txpipe_clp
+export PYTHONPATH=/sps/lsst/users/ebarroso/CLPipeline:$PYTHONPATH
+export PYTHONPATH=/sps/lsst/users/ebarroso/TXPipe:$PYTHONPATH
+
+ceci TXPipe_ingest.yml
